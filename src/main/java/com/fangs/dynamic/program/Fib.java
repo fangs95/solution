890d9d098188227fe719e5849d1ce97bb896c887
fangs <fangshu163@163.com>
2023-05-01 14:28:31 +0800

    509. 斐波那契数

---
+++ src/main/java/com/fangs/dynamic/program/Fib.java
@@ -0,0 +1,25 @@
+package com.fangs.dynamic.program;
+
+/**
+ * 509. 斐波那契数
+ * 斐波那契数 （通常用 F(n) 表示）形成的序列称为 斐波那契数列 。该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
+ * F(0) = 0，F(1) = 1
+ * F(n) = F(n - 1) + F(n - 2)，其中 n > 1
+ * 给定 n ，请计算 F(n) 。
+ *
+ * @author fangs
+ */
+public class Fib {
+    public int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; i++) {
+            int t = a + b;
+            a = b;
+            b = t;
+        }
+        return b;
+    }
+}
